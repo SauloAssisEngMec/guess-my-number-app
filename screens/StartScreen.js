5a61cbcf2757b1085d1581b1773bd4de5b1dc7d3
saulo <sauloassis4123@gmail.com>
2024-01-19 17:34:46 -0300

    style: setup startScreen

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Confirm</PrimaryButton>
       <PrimaryButton>Reset</PrimaryButton>
@@ -11,3 +11,18 @@ function StartScreen() {
   );
 }
 export default StartScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    backgroundColor: "#662549",
+    marginHorizontal: 22,
+    padding: 16,
+    borderRadius: 8,
+    elevation: 5,
+    shadowColor: "black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.4,
+    shadowRadius: 6,
+  },
+});
