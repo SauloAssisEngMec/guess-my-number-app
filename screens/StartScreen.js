1ca33d74aa2b1a91ae8e21b3f661b4ddf8f22b3c
saulo <sauloassis4123@gmail.com>
2024-01-20 20:12:18 -0300

    Styling and configuring textInput

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -4,7 +4,11 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
       <PrimaryButton>Confirm</PrimaryButton>
       <PrimaryButton>Reset</PrimaryButton>
     </View>
@@ -24,5 +28,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 2, height: 2 },
     shadowOpacity: 0.4,
     shadowRadius: 6,
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#fff",
+    height: 50,
+    width: 60,
+    fontSize: 30,
+    color: "#fff",
+    marginVertical: 10,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
